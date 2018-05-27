79b0abb349426bad1c982b0d298aae3498ea843c
Gabriel Cossette <gabriel@gcossette.com>
2018-05-27 19:10:54 -0400

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,16 @@
-FROM ubuntu:trusty
+FROM ubuntu:xenial
 RUN apt-get update && apt-get upgrade -y && apt-get install -y ed ssh rsyslog fail2ban openssh-server openssh-client supervisor python-pyinotify libpam-google-authenticator && apt-get clean
 ENV DEBIAN_FRONTEND noninteractive
 
 # Enable google-auth
 RUN sed -i '2i auth required pam_google_authenticator.so' /etc/pam.d/sshd
 RUN sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
-RUN adduser user --disabled-login && adduser user adm && adduser user sudo
 
 # Set up directories
-RUN mkdir -p /var/run/sshd /var/log/supervisor /var/run/fail2ban /root/.ssh /home/user/.ssh
+RUN mkdir -p /var/run/sshd /var/log/supervisor /var/run/fail2ban
 
 COPY fail2ban-supervisor.sh /usr/local/bin/
 COPY supervisor.d/* /etc/supervisor/conf.d/
 COPY fail2ban/* /etc/fail2ban/
-RUN chown -R user: /home/user/
-CMD ["/usr/bin/supervisord","-c","/etc/supervisor/supervisord.conf"] 
+CMD ["entrypoint.sh"] 
 EXPOSE 22
---
+++ entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set +e
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env '$SFTP_USER1'
+file_env '$SFTP_USER2'
+file_env '$SFTP_USER3'
+file_env '$SFTP_PASSWORD1'
+file_env '$SFTP_PASSWORD2'
+file_env '$SFTP_PASSWORD3'
+file_env '$SFTP_SECRET1'
+file_env '$SFTP_SECRET2'
+file_env '$SFTP_SECRET3'
+
+if [ ! -f /first_run_passed ]; then
+    adduser $SFTP_USER1 --disabled-login
+    adduser $SFTP_USER2 --disabled-login
+    adduser $SFTP_USER3 --disabled-login
+    mkdir -p /home/$SFTP_USER1/.ssh /home/$SFTP_USER2/.ssh /home/$SFTP_USER3/.ssh
+    touch /first_run_passed
+fi
+
+cp /google_authenticator /home/$SFTP_USER1/.google_authenticator
+cp /google_authenticator /home/$SFTP_USER2/.google_authenticator
+cp /google_authenticator /home/$SFTP_USER3/.google_authenticator
+
+sed -i -e "s/SECRETUSER/$SFTP_SECRET1/g" /home/$SFTP_USER1/.google_authenticator
+sed -i -e "s/SECRETUSER/$SFTP_SECRET2/g" /home/$SFTP_USER2/.google_authenticator
+sed -i -e "s/SECRETUSER/$SFTP_SECRET3/g" /home/$SFTP_USER3/.google_authenticator
+
+chown -R $SFTP_USER1: /home/$SFTP_USER1
+chown -R $SFTP_USER2: /home/$SFTP_USER2
+chown -R $SFTP_USER3: /home/$SFTP_USER3
+
+supervisord -c /etc/supervisor/supervisord.conf
